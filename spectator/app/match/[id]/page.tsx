ecb72e99b7f52116a87e5553c56aa94df4bf593e
Safique Ahmed Faruque <69505988+s-a-faruque@users.noreply.github.com>
2024-08-30 20:17:56 +0000

    improve using access token

--- spectator/app/match/[id]/page.tsx
+++ spectator/app/match/[id]/page.tsx
@@ -27,6 +27,7 @@ export default function Home({ params }: { params: Params }) {
   const [data, setData] = useState<ApiResponse | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
+  const [accessToken, setAccessToken] = useState<string | null>();
 
   useEffect(() => {
     
@@ -40,35 +41,76 @@ export default function Home({ params }: { params: Params }) {
           matchId: id
         }
       };
-
-      try {
-        const response = await fetch('https://us-east-1.aws.data.mongodb-api.com/app/data-gdwsjkb/endpoint/data/v1/action/findOne', {
+      const accessTokenFromLocalStorage = localStorage.getItem('accessToken')
+      if (accessTokenFromLocalStorage){
+        console.log(accessTokenFromLocalStorage);
+        setAccessToken(accessTokenFromLocalStorage);
+      } else {
+        const response = await fetch('https://realm.mongodb.com/api/client/v2.0/app/data-gdwsjkb/auth/providers/api-key/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
             'Access-Control-Request-Headers': '*',
-            'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJiYWFzX2RldmljZV9pZCI6IjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMCIsImJhYXNfZG9tYWluX2lkIjoiNjZjOGUzMjIzODE5YTQ2YTczNGRhOTI0IiwiZXhwIjoxNzI0NjEzMDgzLCJpYXQiOjE3MjQ2MTEyODMsImlzcyI6IjY2Y2I3YWQzZTAwY2YxM2Q0MDViZmE3NSIsImp0aSI6IjY2Y2I3YWQzZTAwY2YxM2Q0MDViZmE3OCIsInN0aXRjaF9kZXZJZCI6IjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMCIsInN0aXRjaF9kb21haW5JZCI6IjY2YzhlMzIyMzgxOWE0NmE3MzRkYTkyNCIsInN1YiI6IjY2YzhlMzQ2YTg5ZjFlZmNiYmJmMThhNSIsInR5cCI6ImFjY2VzcyJ9.W8-8Cvbd6PLaTxxMSgU8Y_0uBr3yd-pYTgrZZ6qkj_Y',
-            // 'api-key': 'szh5JljBccYCUJHBEUCrwDscVhwLsfv0pHUTw4iGtuhFrKiDkD9KFepQLhCVoSxW',
           },
-          body: JSON.stringify(body),
+          body: JSON.stringify({
+            'key': 'szh5JljBccYCUJHBEUCrwDscVhwLsfv0pHUTw4iGtuhFrKiDkD9KFepQLhCVoSxW',
+          }),
         });
+        const authentication = await response.json();
+        console.log(authentication);
+        localStorage.setItem('accessToken', authentication.access_token ?? null);
+        setAccessToken(authentication.access_token);
+      }
+      /*
+        1. Is there any accessToken in localStorage? 
+          if yes, use it.
+          if no, 
+            1. do an initial authentication
+            2. store in localStorage
+            3. use it. ex. setAccessToken(accessToken)
+      */
 
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
+      //if(accessTokenFromLocalStorage){
+      if(typeof accessToken !== 'undefined'){
+        
+        console.log(accessTokenFromLocalStorage);
+
+        try {
+          const response = await fetch('https://us-east-1.aws.data.mongodb-api.com/app/data-gdwsjkb/endpoint/data/v1/action/findOne', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Access-Control-Request-Headers': '*',
+              //'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJiYWFzX2RldmljZV9pZCI6IjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMCIsImJhYXNfZG9tYWluX2lkIjoiNjZjOGUzMjIzODE5YTQ2YTczNGRhOTI0IiwiZXhwIjoxNzI1MDQ5MjUyLCJpYXQiOjE3MjUwNDc0NTIsImlzcyI6IjY2ZDIyMjljYjc0YzExZjFlOWVjYWJmZSIsImp0aSI6IjY2ZDIyMjljYjc0YzExZjFlOWVjYWMwMSIsInN0aXRjaF9kZXZJZCI6IjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMCIsInN0aXRjaF9kb21haW5JZCI6IjY2YzhlMzIyMzgxOWE0NmE3MzRkYTkyNCIsInN1YiI6IjY2YzhlMzQ2YTg5ZjFlZmNiYmJmMThhNSIsInR5cCI6ImFjY2VzcyJ9.gSOJ5Fp2KBLQPC5_GW2XOlyQpu0wXhpNDTcZsGzVPoo',
+              //'Authorization': 'Bearer ' + accessTokenFromLocalStorage,
+              'Authorization': 'Bearer ' + accessToken,
+              // 'api-key': 'szh5JljBccYCUJHBEUCrwDscVhwLsfv0pHUTw4iGtuhFrKiDkD9KFepQLhCVoSxW',
+            },
+            body: JSON.stringify(body),
+          });
+  
+          // if response is about expired token
+          // get accessToken with refresh token
+          // call API with this new accessToken
+          if (!response.ok) {
+            throw new Error('Failed to fetch data');
+          }
+  
+          const result: ApiResponse = await response.json();
+          setData(result);
+        } catch (err: any) {
+          console.error('Error fetching data:', err.message);
+          setError(err.message);
+        } finally {
+          setLoading(false);
         }
 
-        const result: ApiResponse = await response.json();
-        setData(result);
-      } catch (err: any) {
-        console.error('Error fetching data:', err.message);
-        setError(err.message);
-      } finally {
-        setLoading(false);
       }
+      
     };
 
     fetchData();
-  }, []);
+  }, [accessToken]);
   
 
   if (error) return <div>Failed to load data 44 `{error}`</div>;
