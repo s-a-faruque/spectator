6f3c1cd294864d37d5e6284f05a9a8ad05e29748
Safique Ahmed Faruque <69505988+s-a-faruque@users.noreply.github.com>
2025-04-21 02:37:42 +0000

    display link of matches

--- spectator/app/badminton/scorer/page.tsx
+++ spectator/app/badminton/scorer/page.tsx
@@ -90,14 +90,18 @@ export default function CardsPage() {
           )}
             
           {matches.map((match, index) => (
-            <div key={index} className={styles.matchItem}>
-                
-                  <div>{match.homePlayerName}</div> <div>{match.homePlayerScore} : {match.awayPlayerScore} </div> <div>{match.awayPlayerName}</div>
-                  <div><Link href="/badminton/scorer/match/[id]" as={`/badminton/scorer/match/${match.id}`}><Image src="/arrow-right.png" alt="Logout" width={16} height={16} /></Link></div>
-              {/* <button className={styles.clearMatchesButton} onClick={(index) => {clearMatches()}} style={{ float: "right" }}>
-                <Image src="/bin.png" alt="Logout" width={16} height={16} />
-              </button> */}
-            </div>
+            <Link href={`/badminton/scorer/match/${match.id}`} className={styles.matchItemLink} passHref>
+                <div key={index} className={styles.matchItem} style={{ cursor: 'pointer' }}>
+                  <div>{match.homePlayerName}</div>
+                  <div><strong>{match.homePlayerScore}</strong> : <strong>{match.awayPlayerScore}</strong></div>
+                  <div>{match.awayPlayerName}</div>
+                </div>
+            </Link>
+
+            // <div key={index} className={styles.matchItem}>
+            //   <div>{match.homePlayerName}</div> <div>{match.homePlayerScore} : {match.awayPlayerScore} </div> <div>{match.awayPlayerName}</div>
+            //   <div><Link href="/badminton/scorer/match/[id]" as={`/badminton/scorer/match/${match.id}`}><Image src="/arrow-right.png" alt="Logout" width={16} height={16} /></Link></div>
+            // </div>
           ))}
         </div>
       </div>
