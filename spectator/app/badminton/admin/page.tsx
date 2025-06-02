a99ccbcfe6eb318668b7c7b0bea3ff1217b0e721
Safique Ahmed Faruque <69505988+s-a-faruque@users.noreply.github.com>
2025-06-02 19:19:33 -0300

    Update page.tsx

--- spectator/app/badminton/admin/page.tsx
+++ spectator/app/badminton/admin/page.tsx
@@ -3,9 +3,26 @@ import TournamentTeamManager from './components/TournamentTeamManager';
 
 export default function AdminPage() {
   return (
-    <main>
-      <h1>Admin Dashboard</h1>
-      <TournamentTeamManager />
+    <main className={styles.main}>
+      <header className={styles.header}>
+        <h1>
+          <Link className={styles.home} href="/">
+              <Image
+                src="/homepage.png"
+                alt="Home Icon"
+                width={16}
+                height={16}
+              />
+          </Link>
+          Admin Home
+        </h1>
+        
+      </header>
+      <div className={styles.primaryContent}>
+        <div className={styles.matchesList}>
+            <TournamentTeamManager />
+        </div>
+      </div>
     </main>
   );
 }
